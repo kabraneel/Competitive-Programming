fff859eb07a8d4bfdb2e39d2c0dc6e40cca67683
Neel Kabra <kabraneel@gmail.com>
2021-08-18 19:11:21 +0530

    hi

---
+++ STARTERS9/C.cpp
@@ -0,0 +1,92 @@
+// na ho paega humse
+// nai_aati_coding
+// i love PD
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+#define int long long
+#define PB push_back
+#define all(x) (x).begin(),(x).end()
+#define MP make_pair
+#define sz(x) (int)(x.size())
+
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+void solve(){
+  int n, k; cin>>n>>k;
+  string s; cin>>s;
+
+  vector<int> ones;
+  int ans = 1;
+
+  for(int i = 0; i<n; i++){
+    if(s[i] == '1') ones.PB(i);
+  }
+
+  if(!sz(ones)){
+    cout<<"0\n";
+    return;
+  }
+
+  int n1 = sz(ones);
+
+  ones[0]++;
+
+  for(int i = 0; i<n1-1; i++){
+
+    if(ones[i+1] - ones[i] < k){
+      ones[i+1]++;
+      continue;
+    }
+
+    else if(ones[i+1] - ones[i] == k){
+      continue;
+    }
+
+    else if(ones[i+1] - ones[i] == k+1){
+      ones[i+1]--;
+    }
+
+    else{
+      ans++;
+      ones[i+1]++;
+    }
+
+  }
+
+  cout<<ans<<'\n';
+
+}
+
+signed main(){
+
+  #ifndef ONLINE_JUDGE
+  freopen("/home/kabraneel/coding/inputfa.txt", "r", stdin);
+  freopen("/home/kabraneel/coding/outputfa.txt", "w", stdout);
+  freopen("/home/kabraneel/coding/error.txt","w",stderr);
+  #endif
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  auto start = chrono::high_resolution_clock::now();
+
+  int t;
+  cin>>t;
+  while(t--){
+    solve();
+  }
+
+  auto end = chrono::high_resolution_clock::now();
+  double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+  time_taken *= 1e-9;
+
+  cerr <<fixed<<time_taken<<setprecision(9)<< " sec"<<endl;
+  return 0;
+}
